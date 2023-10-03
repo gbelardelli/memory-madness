74fded7758ee5a171119aa2ca05b6445a9d841cf
Gianluca <gbelardelli@gmail.com>
2023-10-03 19:16:29 +0200

    sound & game manager

---
+++ level_button/level_button.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+@onready var label = $Label
+@onready var sound = $Sound
+
+var _level_number: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func set_level_number(level_num:int) -> void:
+	_level_number = level_num
+	var lvl_data = GameManager.LEVELS[_level_number]
+	label.text = "%sx%s" % [lvl_data.rows, lvl_data.cols]
+
+
+func _on_pressed():
+	SoundManager.play_button_click(sound)
